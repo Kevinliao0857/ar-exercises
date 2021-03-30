690df2f365c19d7008c346a280b40d030ab84450
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-30 00:32:19 +0000

    slight refactor

--- lib/store.rb
+++ lib/store.rb
@@ -7,6 +7,6 @@ class Store < ActiveRecord::Base
 
   validates :name, length: { minimum: 3}
 
-  validates :annual_revenue, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 end
