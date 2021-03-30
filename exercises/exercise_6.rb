90ca0a332753839111e96ed53d895633d7ea1e22
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-30 00:12:48 +0000

    changing the hourly rates

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -18,10 +18,10 @@ puts "----------"
 # 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
 # 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
 
-@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Kevin", last_name: "Liao", hourly_rate: 60)
-@store1.employees.create(first_name: "Bobby", last_name: "Lee", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 180)
+@store1.employees.create(first_name: "Kevin", last_name: "Liao", hourly_rate: 160)
+@store1.employees.create(first_name: "Bobby", last_name: "Lee", hourly_rate: 45)
 
-@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
 @store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
-@store2.employees.create(first_name: "Rick", last_name: "Astley", hourly_rate: 60)
+@store2.employees.create(first_name: "Rick", last_name: "Astley", hourly_rate: 190)
